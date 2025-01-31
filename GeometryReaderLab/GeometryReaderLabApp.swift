04b2688bab093248005d0054dde97043003eb88c
PortableDoughnut <gwen.thelin@gmail.com>
2025-01-31 09:33:54 -0700

    Initial Commit

---
+++ GeometryReaderLab/GeometryReaderLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderLabApp.swift
+//  GeometryReaderLab
+//
+//  Created by Gwen Thelin on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
